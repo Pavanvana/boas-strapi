446cf77650808d1488f8fccc440fba961b3b4157
Pavan <vana.pavankalyan1999@gmail.com>
2024-03-13 12:21:37 +0530

    update database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,11 +2,14 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env(
+        "DATABASE_HOST",
+        "dpg-cno844i0si5c73b378t0-a.oregon-postgres.render.com"
+      ),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
+      database: env("DATABASE_NAME", "boasdb"),
+      user: env("DATABASE_USERNAME", "admin"),
+      password: env("DATABASE_PASSWORD", "4TGeWHq7kMJ4Zxr16c0Hqb9gFthjdOV8"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
         ca: env("DATABASE_CA"),
